27d1c5c601972ef7110f30375d150dfbfc32bb37
Jesss <108489705+JVillasBoas@users.noreply.github.com>
2023-09-30 01:09:13 -0300

    obs: começar js resolver erros

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
+class time {
+    constructor(nome,pontuação) {
+        this.nome = nome;
+        this.pontos = pontuação;
+    }
+    mostrar() {
+        let local = document.querySelector("#menu-meio");
+        let divisoria = document.createElement("div");
+        divisoria.className("nomeando-times");
+        let label = document.createElement("label");
+        divisoria.appendChild(label);
+        label.innerHTML = "Insert Team`s Name";
+        let texto = document.createElement("input");
+        divisoria.appendChild(texto);
+        texto.setAttribute("type","text");
+        texto.className("input-name");
+        local.appendChild(divisoria);
+    }
+}
 
+function novo() {
+    let c = document.querySelector("#quantidade-input");
+    let local = document.querySelector("#menu-meio");
+    local.appendChild(c);
+    local.innerHTML = "oioioioi";
+}
+
+
+let botao = document.querySelector("#botao-inicial2");
+botao.addEventListener("click",novo);
