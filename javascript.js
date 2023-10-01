0a5c84e6cee768d00ee4ec5b96c208b6907271b4
Jesss <108489705+JVillasBoas@users.noreply.github.com>
2023-10-01 01:31:18 -0300

    obs: coisas saindo p fora da tela

--- javascript.js
+++ javascript.js
@@ -3,28 +3,5 @@ class time {
         this.nome = nome;
         this.pontos = pontuação;
     }
-    mostrar() {
-        let local = document.querySelector("#menu-meio");
-        let divisoria = document.createElement("div");
-        divisoria.className("nomeando-times");
-        let label = document.createElement("label");
-        divisoria.appendChild(label);
-        label.innerHTML = "Insert Team`s Name";
-        let texto = document.createElement("input");
-        divisoria.appendChild(texto);
-        texto.setAttribute("type","text");
-        texto.className("input-name");
-        local.appendChild(divisoria);
-    }
+   
 }
-
-function novo() {
-    let c = document.querySelector("#quantidade-input");
-    let local = document.querySelector("#menu-meio");
-    local.appendChild(c);
-    local.innerHTML = "oioioioi";
-}
-
-
-let botao = document.querySelector("#botao-inicial2");
-botao.addEventListener("click",novo);
