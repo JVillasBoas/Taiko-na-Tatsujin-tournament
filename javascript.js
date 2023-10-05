012d042df81062d5ae9ed88f1745bf0670d75783
Jesss <108489705+JVillasBoas@users.noreply.github.com>
2023-10-05 00:59:23 -0300

    h

--- javascript.js
+++ javascript.js
@@ -41,30 +41,82 @@ function  mostrar() {
     }
 }
 
+//minhas alterações
+
+function Resultado() {
+    for (c = 0; c < numero_disputa; c++) { //aqui coloquei zero pra dar o tamanho e tirei o <= do disputa pra so <
+        let selecionar = document.querySelector("#input-options" + c).value;
+        // let valor = selecionar.options[selecionar.selectedIndex].value;
+
+        for (j = 0; j < times.length; j++) {
+            if (selecionar == times[j].nome) {
+                times[j].quantidade_vitorias += 1;
+            }
+        }
+    }
+}
+
+//
+
 class time {
-    constructor(nome,classificação) {
+    constructor(nome,quantidade_vitorias) {
         this.nome = nome;
-        this.classific = classificação;
+        this.quantidade_vitorias = quantidade_vitorias;
     }
 }
 const times = [];
-var testando = [];
+//var testando = [];
+var numero_disputa = 0;
 function start() {
     for(c=1;c<=quantidade.value;c++) {
         let nome_time = document.querySelector('#texto' + c);
-        times.push(new time(nome_time.value));
+        times.push(new time(nome_time.value, 0));
     }
     let local = document.querySelector("#menu-inicial");
     local.innerHTML = "";
     let imagem = document.createElement("img");
-    imagem.setAttribute("src","Logo-menu-meio.png");
-    imagem.setAttribute("id","titulo-menu2");
+    imagem.setAttribute("src","nuvem.png");
+    imagem.setAttribute("id","titulo-menu3");
     local.appendChild(imagem);
-    for(c=0;c<=quantidade.value-1;c++) {
-        for(j=c+1;j<=quantidade.value-1;j++) {
-            testando.push(times[j].nome + "X" + times[c].nome);
+    
+    for(c=0;c<quantidade.value-1;c++) {
+        for(j=1;j<quantidade.value;j++) {
+            let divisoria = document.createElement("div");
+            divisoria.setAttribute("class","jogo");
+            let texto = document.createElement("div");
+            texto.setAttribute("class","times-disputa");
+            texto.innerHTML = times[c].nome + "X" + times[j].nome;
+            let selecionar = document.createElement("select");
+            selecionar.setAttribute("id","input-options" + c); //aqui nao pode ser j porque ele vai se repetir para os outros inputs
+            selecionar.setAttribute("class","input-options"); 
+            let options1 = document.createElement("option");
+            let options2 = document.createElement("option");
+            options1.innerHTML = times[j].nome;
+            options2.innerHTML = times[c].nome;
+            selecionar.appendChild(options1);
+            selecionar.appendChild(options2);
+            divisoria.appendChild(texto);
+            divisoria.appendChild(selecionar);
+            local.appendChild(divisoria);
+            numero_disputa++
+            //testando.push(times[j].nome + "X" + times[c].nome);
         }
     }
+    let botao = document.createElement("input");
+    botao.setAttribute("type","button");
+    botao.setAttribute("id","botao-resultado");
+    botao.setAttribute("value","Outcome");
+
+    //minhas alterações
+
+    botao.setAttribute("onclick", "Resultado()");
+
+    //
+
+    //comecarTorneio.setAttribute("onClick","resultado()");
+    local.appendChild(botao);
 }
+
+
 
 
