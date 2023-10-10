0bb376d2f7cb3c91247e2e294cf9ec542bc8c76f
Jesss <108489705+JVillasBoas@users.noreply.github.com>
2023-10-10 19:10:39 -0300

    local storage

--- javascript.js
+++ javascript.js
@@ -11,10 +11,12 @@ function novaTela() {
     document.body.style.backgroundImage = "url('wallpaper2.png')";
     document.body.style.height = "max-content";
 } 
+
 var quantidade;
-function  mostrar() {
+
+function gerarTelaNomear() {
     quantidade = document.querySelector("#quantidade-input");
-    if (quantidade.value > 20 || quantidade.value < 3) {
+    if (quantidade.value > 20 || quantidade.value < 2) {
         alert("Quantidade não suportada.");
     } else {
         for(c=1;c<=quantidade.value;c++) {
@@ -35,16 +37,53 @@ function  mostrar() {
         comecarTorneio.setAttribute("type","button");
         comecarTorneio.setAttribute("value","Start");
         comecarTorneio.setAttribute("id","botao-start");
-        comecarTorneio.setAttribute("onClick","start()");
+        comecarTorneio.setAttribute("onClick","gerarTelaDisputa()");
         local.appendChild(comecarTorneio); 
         novaTela();
     }
 }
+
+function gerarTelaRanking() {
+    resultadoJogo();
+    let local = document.querySelector("#menu-inicial");
+    local.innerHTML = "";
+    let imagem = document.createElement("img");
+    imagem.setAttribute("src","taikogif.gif");
+    imagem.setAttribute("id","titulo-menu3");
+    local.appendChild(imagem);
+    let titulo = document.createElement("div");
+    titulo.innerHTML = "Final Ranking - Taiko no Tatsujin";
+    let tabela = document.createElement('table');
+    let tipos = document.createElement('tr');
+    let tipo1 = document.createElement('th');
+    tipo1.innerHTML = "Teams";
+    let tipo2 = document.createElement('th');
+    tipo2.innerHTML = "Wins";
+    tipos.appendChild(tipo1);
+    tipos.appendChild(tipo2);
+    local.appendChild(titulo);
+    tabela.appendChild(tipos);
+    
+    for(c=0;c<quantidade.value;c++) {
+        let time = document.createElement('tr');
+        let nome = document.createElement('td');
+        nome.innerHTML = times[c].nome;
+        let vitorias = document.createElement('td');
+        vitorias.innerHTML = times[c].quantidade_vitorias;
+        time.appendChild(nome);
+        time.appendChild(vitorias);
+        tabela.appendChild(time);
+    }
+
+    local.appendChild(tabela)
+}
+
 var vencedor
 var vencedores = [];
 var pos_vencedor;
 var vitorias = [];
-function Resultado() {
+
+function resultadoJogo() {
     for (c = 0; c < numero_disputa; c++) { 
 
         let selecionar = document.querySelector("#input-options" + c).value;
@@ -56,7 +95,14 @@ function Resultado() {
         }
     }
 
-    for(y=0;y<quantidade.value;y++) {
+    times.sort(function(a, b) {
+        if(a.quantidade_vitorias > b.quantidade_vitorias) {
+          return -1;
+        } else {
+          return true;
+        }
+      });
+    /* for(y=0;y<quantidade.value;y++) {
         vitorias.push(times[y].quantidade_vitorias);
     }
     
@@ -77,10 +123,7 @@ function Resultado() {
                 vencedores.push(times[c].nome);
             }
         }
-    }
-
-
-
+    } */
 }
 
 class time {
@@ -93,25 +136,31 @@ class time {
 const times = [];
 var numero_disputa = 0;
 
-function start() {
-    for(c=1;c<=quantidade.value;c++) {
-        let nome_time = document.querySelector('#texto' + c);
-        times.push(new time(nome_time.value, 0));
-    }
+function gerarTelaDisputa() {
+    combinarTimes();
     let local = document.querySelector("#menu-inicial");
     local.innerHTML = "";
     let imagem = document.createElement("img");
     imagem.setAttribute("src","nuvem.png");
     imagem.setAttribute("id","titulo-menu3");
     local.appendChild(imagem);
-    
     for(c=0;c<quantidade.value-1;c++) {
         for(j=c+1;j<quantidade.value;j++) {
             let divisoria = document.createElement("div");
+            let divisoria2 = document.createElement("div");
+            let divisoria3 = document.createElement("div");
+            divisoria3.innerHTML = 'Select the winner:'
+            divisoria2.setAttribute("class","disputas");
             divisoria.setAttribute("class","jogo");
             let texto = document.createElement("div");
             texto.setAttribute("class","times-disputa");
-            texto.innerHTML = times[c].nome + "X" + times[j].nome;
+            texto.innerHTML = times[c].nome;
+            let texto2 = document.createElement("div");
+            texto2.setAttribute("class","x-disputa");
+            texto2.innerHTML = "X";
+            let texto3 = document.createElement("div");
+            texto3.setAttribute("class","times-disputa");
+            texto3.innerHTML = times[j].nome;
             let selecionar = document.createElement("select");
             selecionar.setAttribute("id","input-options" + numero_disputa); 
             selecionar.setAttribute("class","input-options"); 
@@ -121,18 +170,30 @@ function start() {
             options2.innerHTML = times[c].nome;
             selecionar.appendChild(options1);
             selecionar.appendChild(options2);
-            divisoria.appendChild(texto);
-            divisoria.appendChild(selecionar);
+            divisoria3.appendChild(selecionar);
+            divisoria2.appendChild(texto);
+            divisoria2.appendChild(texto2);
+            divisoria2.appendChild(texto3);
+            divisoria.appendChild(divisoria3);
+            divisoria.appendChild(divisoria2);
             local.appendChild(divisoria);
+            
             numero_disputa++
         }
     }
     let botao = document.createElement("input");
     botao.setAttribute("type","button");
     botao.setAttribute("id","botao-resultado");
-    botao.setAttribute("value","Outcome");
-    botao.setAttribute("onclick", "Resultado()");
+    botao.setAttribute("value","Show");
+    botao.setAttribute("onclick", "gerarTelaRanking()");
     local.appendChild(botao);
+}
+
+function combinarTimes() {
+    for(c=1;c<=quantidade.value;c++) {
+        let nome_time = document.querySelector('#texto' + c);
+        times.push(new time(nome_time.value, 0));
+    }
 }
 
 
